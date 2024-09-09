ed37e948b5ac5d5c9d3eb9f944ef568411d9ebb1
diegolentz <diegoolentz@gmail.com>
2024-09-09 18:17:54 -0300

    libro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LibroComponent } from "./libro/libro.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LibroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
