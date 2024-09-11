95676685aa70d2673e548fae6d275b2543ca8554
ValentinPg <105302328+ValentinPg@users.noreply.github.com>
2024-09-11 17:26:06 -0300

    Inputs de checkbox y radio separados en nuevo componente

---
+++ src/app/shared/input-box/input-box.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-input-box',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './input-box.component.html',
+  styleUrl: './input-box.component.css'
+})
+export class InputBoxComponent {
+  @Input() inputType:string = 'radiobox-input';
+  @Input() label:string = '';
+}
