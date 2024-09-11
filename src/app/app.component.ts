da1f1f5bfdd81c4df26f0ab240860be96c24fe6b
diegolentz <diegoolentz@gmail.com>
2024-09-11 12:53:09 -0300

    resena-valoracion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { LibroComponent } from "./libro/libro.component";  
 import { AmigosComponent } from "./amigos/amigos.component";
 import { ContenedorSectionComponent } from "./contenedor-section/contenedor-section.component";
-
+import { ResenaComponent } from './resena/resena.component';
+import {ValoracionComponent} from './valoracion/valoracion.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AmigosComponent, ContenedorSectionComponent,LibroComponent],
+  imports: [RouterOutlet,ResenaComponent, AmigosComponent, ContenedorSectionComponent,ValoracionComponent,LibroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
