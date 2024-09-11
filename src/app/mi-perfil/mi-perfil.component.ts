162bdcfac18777964696efcb2bdb93afcc841f11
ValentinPg <105302328+ValentinPg@users.noreply.github.com>
2024-09-11 08:50:48 -0300

    Componente de ifromacion del perfil hecho

--- src/app/mi-perfil/mi-perfil.component.ts
+++ src/app/mi-perfil/mi-perfil.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PanelPerfilComponent } from "../panel-perfil/panel-perfil.component";
 import { LibroComponent } from "../libro/libro.component";
+import { PerfilInfoComponent } from "../shared/perfil-info/perfil-info.component";
 
 @Component({
   selector: 'app-mi-perfil',
   standalone: true,
-  imports: [PanelPerfilComponent, LibroComponent],
+  imports: [PanelPerfilComponent, LibroComponent, PerfilInfoComponent],
   templateUrl: './mi-perfil.component.html',
   styleUrl: './mi-perfil.component.css'
 })
