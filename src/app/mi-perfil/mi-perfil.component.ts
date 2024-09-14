a12689a4fec72a5331904d52509a3ebabad7beff
ValentinPg <105302328+ValentinPg@users.noreply.github.com>
2024-09-14 20:06:08 -0300

    HTML y CSS hechos, con iconos en vez de palabras

--- src/app/mi-perfil/mi-perfil.component.ts
+++ src/app/mi-perfil/mi-perfil.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { PanelPerfilComponent } from "../panel-perfil/panel-perfil.component";
 import { LibroComponent } from "../libro/libro.component";
 import { PerfilInfoComponent } from "../shared/perfil-info/perfil-info.component";
+import { BtnGuardarCancelarComponent } from "../shared/btn-guardar-cancelar/btn-guardar-cancelar.component";
 
 @Component({
   selector: 'app-mi-perfil',
   standalone: true,
-  imports: [PanelPerfilComponent, LibroComponent, PerfilInfoComponent],
+  imports: [PanelPerfilComponent, LibroComponent, PerfilInfoComponent, BtnGuardarCancelarComponent],
   templateUrl: './mi-perfil.component.html',
   styleUrl: './mi-perfil.component.css'
 })
---
+++ src/app/shared/btn-guardar-cancelar/btn-guardar-cancelar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-guardar-cancelar',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn-guardar-cancelar.component.html',
+  styleUrl: './btn-guardar-cancelar.component.css'
+})
+export class BtnGuardarCancelarComponent {
+
+}
