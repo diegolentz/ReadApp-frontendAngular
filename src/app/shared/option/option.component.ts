22edb11806ec3a39cf4cc77d23f76961a5c8e879
AdrianPerez0306 <perezadrian.m@hotmail.com>
2024-09-14 20:25:40 -0300

    Pusheo preventivo

--- src/app/shared/option/option.component.ts
+++ src/app/shared/option/option.component.ts
@@ -2,18 +2,15 @@ import { Component, Input } from '@angular/core';
 import { Option } from '../dropdown-menu/dropdown-menu.component';
 import { SvgIconComponent } from 'angular-svg-icon';
 import { NgStyle } from '@angular/common';
-
+import { HoverBoxshadowDirective } from '../directives/hover-boxshadow.directive';
 @Component({
   selector: 'app-option',
   standalone: true,
-  imports: [SvgIconComponent, NgStyle],
+  imports: [SvgIconComponent, NgStyle, HoverBoxshadowDirective],
   templateUrl: './option.component.html',
   styleUrl: './option.component.css'
 })
-
 export class OptionComponent {
   @Input() option!:Option
-
-  hoverColorVar: string = ''
 }
 
