4e7c35491cd00647f87ce9d1c1b6b63710207c2e
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2024-09-20 19:36:02 -0300

    agregando valoraciones individuales

---
+++ src/app/valoracion-individual/valoracion-individual.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ValoracionIndividualComponent } from './valoracion-individual.component';
+
+describe('ValoracionIndividualComponent', () => {
+  let component: ValoracionIndividualComponent;
+  let fixture: ComponentFixture<ValoracionIndividualComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ValoracionIndividualComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ValoracionIndividualComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/valoracion/valoracion.component.ts
+++ src/app/valoracion/valoracion.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BotonAgregarComponent } from '../shared/boton-agregar/boton-agregar.component';
+import { ValoracionIndividualComponent } from "../valoracion-individual/valoracion-individual.component";
 
 @Component({
   selector: 'app-valoracion',
   standalone: true,
-  imports: [BotonAgregarComponent],
+  imports: [BotonAgregarComponent, ValoracionIndividualComponent],
   templateUrl: './valoracion.component.html',
   styleUrl: './valoracion.component.css'
 })
