7ef65e6c721401d46527bb5f6ca004fb28ba64b8
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2024-09-22 19:35:25 -0300

    edit de recomendacion termina, falta mejorar

--- src/app/edit-recommendation/edit-recommendation.component.ts
+++ src/app/edit-recommendation/edit-recommendation.component.ts
@@ -3,13 +3,18 @@ import { HeaderComponent } from '../shared/header/header.component';
 import { ResenaComponent } from '../resena/resena.component';
 import { ProfileBooksToReadComponent } from '../profile-books-to-read/profile-books-to-read.component';
 import { ValoracionComponent } from '../valoracion/valoracion.component';
+import { NgIf } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-edit-recommendation',
   standalone: true,
-  imports: [ResenaComponent,HeaderComponent,ProfileBooksToReadComponent,ValoracionComponent],
+  imports: [NgIf,ResenaComponent,HeaderComponent,ProfileBooksToReadComponent,ValoracionComponent],
   templateUrl: './edit-recommendation.component.html',
   styleUrl: './edit-recommendation.component.css'
 })
 export class EditRecommendationComponent {
-
+  constructor(private router: Router) {}
+  isEditPage() {
+    return this.router.url === '/edit-recommendation'; 
+  }
 }
--- src/app/view-recommendation/view-recommendation.component.ts
+++ src/app/view-recommendation/view-recommendation.component.ts
@@ -5,12 +5,19 @@ import { HeaderComponent } from "../shared/header/header.component";
 import { ResenaComponent } from "../resena/resena.component";
 import { BotonAgregarComponent } from "../shared/boton-agregar/boton-agregar.component";
 import { ValoracionComponent } from '../valoracion/valoracion.component';
+import { NgIf } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-view-recommendation',
   standalone: true,
-  imports: [ProfileBooksReadedComponent, ProfileBooksToReadComponent, HeaderComponent, ResenaComponent, BotonAgregarComponent,ValoracionComponent],
+  imports: [NgIf,ProfileBooksReadedComponent, ProfileBooksToReadComponent, HeaderComponent, ResenaComponent, BotonAgregarComponent,ValoracionComponent],
   templateUrl: './view-recommendation.component.html',
   styleUrl: './view-recommendation.component.css'
 })
 export class ViewRecommendationComponent {
+  constructor(private router: Router) {}
+  isEditPage() {
+    return this.router.url === '/edit-recommendation'; 
+  }
+
 }
