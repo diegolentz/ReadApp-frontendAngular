ca3ef7fa54e904d5887e0c4d705dbbcbca1d0a5b
diegolentz <diegoolentz@gmail.com>
2024-09-23 20:55:01 -0300

    inputs

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,43 +1,113 @@
-import { Component, Input } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators} from '@angular/forms';
+import { NgClass, NgIf } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass, NgIf],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrls: ['./input.component.css'] 
 })
+export class InputComponent implements OnInit {
+  @Input() objeto!: any; 
+  @Input() label: string = "";
+  @Input() id: string = '';
+  @Input() required: boolean = false;
+  formulario!: FormControl; 
 
-export class InputComponent {
-  @Input() inputType:string = 'text';
-  @Input() label:string = '';
-  @Input() id:string = '';
-  @Input() name:string = '';
+  ngOnInit() {
+    this.formulario = this.objeto.iniciar();
+    
+    if (this.formulario.invalid) {
+      this.formulario.markAsTouched();
+    }
 
-  constructor(private formBuilder:FormBuilder){
-    const formulario = this.formBuilder.group({
-      username: ['', [Validators.required]],
-      password: ['', []],
-      email: ['', [Validators.required, Validators.email]],
-      texto: ['', []],
-      fecha: ['', [Validators.required, Validador.dateValidator]],
-      numeros: [0, []]
-    })
+    //contenido del input
+    this.formulario.valueChanges.subscribe(value => {
+      console.log('Valor ingresado en el input:', value);
+  
+    });
+  }
+
+//  parte turbina
+  getErrorMessage(): string {
+    if (this.formulario.hasError('required')) {
+      return 'Este campo es obligatorio.';
+    }
+    // validaciones del pattern / exp regulares
+    if (this.formulario.hasError('pattern')) {
+      // si el campo es username
+      if (this.objeto.inputType() === "text" || this.objeto.inputType() === "password") {
+        return 'Este campo solo puede contener letras y números.';
+      }
+      // si el campo es email
+      if (this.objeto.inputType() === "mail") {
+        return 'El correo debe contener un formato válido (ej: usuario@dominio.com).';
+      }
+      // si el campo es numero
+      if (this.objeto.inputType() === "number") {
+        return 'Por favor, introduce un número positivo.';
+      }
+    }
+  
+    if (this.formulario.hasError('minlength')) {
+      const minLength = this.formulario.getError('minlength').requiredLength;
+      return `Este campo debe contener al menos ${minLength} caracteres.`;
+    }
+  
+    if (this.formulario.hasError('maxlength')) {
+      const maxLength = this.formulario.getError('maxlength').requiredLength;
+      return `Este campo debe contener como máximo ${maxLength} caracteres.`;
+    }
+  
+    return ''; 
   }
 }
+  
 
 
-class Validador{
-
-  MIN_CH_USUARIO:number = 5;
-
-  static dateValidator(control:FormControl): ValidationErrors | null{
-    let hoy : Date = new Date();
-       if (new Date(control.value) < hoy)
-           return { "dateValidator": true };
-       return null;
-   }
-
-  }
-
+interface FormBase {
+  iniciar(): FormControl;
+  inputType(): string;
+  estilos() : string;
+}
+export const Username: FormBase = {
+  
+  iniciar: () => new FormControl('', [Validators.required, Validators.minLength(3),Validators.maxLength(15),Validators.pattern('^[a-zA-Z0-9]+$')]),
+  inputType: () => "text",
+  estilos: () => "usuario",
+};
+export const Password: FormBase = {
+  iniciar: () => new FormControl('',[Validators.required, Validators.minLength(8),Validators.maxLength(10),Validators.pattern('^[a-zA-Z0-9]+$')]),
+  inputType: () => "password",
+  estilos:() => "password"
+};
+export const Email: FormBase = {
+  iniciar: () => new FormControl('',[Validators.minLength(3),Validators.maxLength(30),Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.com$')]),
+  inputType: () => "mail",
+  estilos:() => "email"
+};
+export const Texto: FormBase = {
+  iniciar: () => new FormControl('',[Validators.minLength(30),Validators.maxLength(500)]),
+  inputType: () => "textarea",
+  estilos:() => "texto"
+};
+// pipes?
+export const Fecha: FormBase = {
+  iniciar: () => new FormControl('',[Validators.minLength(3),Validators.maxLength(30)]),
+  inputType: () => "date",
+  estilos:() => "fecha"
+};
+// 
+export const Numero: FormBase = {
+  iniciar: () => new FormControl('',[Validators.minLength(1),Validators.maxLength(15),Validators.pattern('^[1-9][0-9]*$')]),
+  inputType: () => "number",
+  estilos:() => "numero"
+};
+// letras y numeros
+export const Nav: FormBase = {
+  iniciar: () => new FormControl('',[Validators.minLength(3),Validators.maxLength(30),Validators.pattern('^[a-zA-Z0-9]+$')]),
+  inputType: () => "text",
+  estilos:() => "nav"
+};
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EncabezadoComponent } from '../shared/encabezado/encabezado.component';
+import { InputComponent } from '../input/input.component';
+
+import { Username,Password } from '../input/input.component';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [EncabezadoComponent],
+  imports: [EncabezadoComponent, InputComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  username = Username
+  password = Password
   constructor(private router:Router, private route:ActivatedRoute){}
   
   goTo(option:string){
