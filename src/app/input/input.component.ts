7bdef0c5b7b56d96b7d5e91c9c1351722acc276d
diegolentz <diegoolentz@gmail.com>
2024-09-25 20:19:03 -0300

    inputs terminados

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,23 +1,49 @@
-import { NgClass, NgIf } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
-  standalone: true,
-  imports: [ReactiveFormsModule, NgClass, NgIf],
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css'] 
+  styleUrls: ['./input.component.css'],
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements ControlValueAccessor {
   @Input() label: string = "";
   @Input() id: string = '';
-  @Input() inputType !: string
+  @Input() inputType: string = 'text';
   @Input() required: boolean = false;
 
-  ngOnInit() {
-   
-  }
-}
-  
+  value: string = ''; // El valor que controla el input
+  onChange: any = () => {};
+  onTouched: any = () => {};
 
+  // cuando el valor del input cambia
+  onInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.value = input.value;
+    this.onChange(this.value); // avisa del cambio de valor a Angular
+    this.onTouched();
+  }
+
+  // ControlValueAccessor implementation
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+
+}
--- src/app/shared/perfil-info/perfil-info.component.ts
+++ src/app/shared/perfil-info/perfil-info.component.ts
@@ -2,25 +2,33 @@ import { Component,HostBinding } from '@angular/core';
 import { InputBoxComponent } from "../input-box/input-box.component";
 import { CommonModule } from '@angular/common';
 import { InputComponent } from '../../input/input.component';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
 
 
 @Component({
   selector: 'app-perfil-info',
   standalone: true,
-  imports: [InputBoxComponent, CommonModule, InputComponent],
+  imports: [InputBoxComponent, CommonModule, InputComponent,FormsModule],
   templateUrl: './perfil-info.component.html',
   styleUrl: './perfil-info.component.css'
 })
 export class PerfilInfoComponent {
   @HostBinding('style.width') width: string = '100%';
-  // username = Username;
-  // nombreApellido = NombreApellido;
-  // password = Password;
-  // email = Email;
-  // texto = Texto;
-  // fecha = Fecha;
+
   mostrarCalculador = new MostrarCalculador();
+  texto : string = "";
   numero !: string;
+  mostrarNuevosInputs: boolean = false; 
+
+  ngOnInit() {
+    console.log(this.texto);
+  }
+  mostrar(event: any) {
+    this.mostrarNuevosInputs = event.target.checked; // Actualiza según si el checkbox está marcado o no
+  }
+
 }
 
 class MostrarCalculador{
