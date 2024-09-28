4c4446bfa769c40a391b68fb3ff3ba5366f72b1f
diegolentz <diegoolentz@gmail.com>
2024-09-28 11:02:42 -0300

    funcionando hay qe mejorar

--- src/app/busqueda-libros/busqueda-libros.component.ts
+++ src/app/busqueda-libros/busqueda-libros.component.ts
@@ -1,25 +1,37 @@
-import { Component,  } from '@angular/core';
+import { Component } from '@angular/core';
+import { BookService } from '../../service/book.service';
+import { Book } from '../../domain/book';
+import { CommonModule, NgFor } from '@angular/common';
 import { HeaderComponent } from '../shared/header/header.component';
 import { NavComponent } from '../nav/nav.component';
 import { ContenedorSectionComponent } from '../contenedor-section/contenedor-section.component';
-import {  LibroComponent } from '../libro/libro.component';
-import { ContainerBooksComponent } from "../shared/layouts/books/books.component";
-import { Book } from '../../domain/book';
-import { BookService } from '../../service/book.service';
-import { CommonModule, NgFor } from '@angular/common';
+import { LibroComponent } from '../libro/libro.component';
+import { ContainerBooksComponent } from '../shared/layouts/books/books.component';
 
 @Component({
   selector: 'app-busqueda-libros',
   standalone: true,
-  imports: [HeaderComponent, LibroComponent, NavComponent, ContenedorSectionComponent,CommonModule, ContainerBooksComponent],
+  imports: [HeaderComponent, LibroComponent, NavComponent, ContenedorSectionComponent, CommonModule, ContainerBooksComponent],
   templateUrl: './busqueda-libros.component.html',
-  styleUrl: './busqueda-libros.component.css'
+  styleUrls: ['./busqueda-libros.component.css']
 })
 export class BusquedaLibrosComponent {
-  
-  constructor(public bookService: BookService) { }
-  books!: Book[];
+  books: Book[] = [];
+
+  constructor(public bookService: BookService) {}
+
   async ngOnInit() {
+    // me traigo los libros
     this.books = await this.bookService.obtenerLibros();
+
+    // me subscribo a los cambios para filtrar
+    this.bookService.filtroCambiado.subscribe(
+      async (nuevoFiltro: string) => {this.books = await this.bookService.obtenerLibros();
+      if (nuevoFiltro) {
+        this.books = this.books.filter((book) =>
+          book.titulo.toLowerCase().includes(nuevoFiltro.toLowerCase())
+        );
+      }
+    });
   }
 }
