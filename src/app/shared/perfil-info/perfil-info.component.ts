9bb0875bb2f9c5b5086f942415858a80f82cd07f
ValentinPg <105302328+ValentinPg@users.noreply.github.com>
2024-09-28 16:52:17 -0300

    Estructura de form y validators hecha en el ts

--- src/app/shared/perfil-info/perfil-info.component.ts
+++ src/app/shared/perfil-info/perfil-info.component.ts
@@ -1,8 +1,8 @@
-import { Component,HostBinding } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { InputBoxComponent } from "../input-box/input-box.component";
 import { CommonModule } from '@angular/common';
 import { InputComponent } from '../../input/input.component';
-import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BtnGuardarCancelarComponent } from '../btn-guardar-cancelar/btn-guardar-cancelar.component';
 
@@ -11,42 +11,66 @@ import { BtnGuardarCancelarComponent } from '../btn-guardar-cancelar/btn-guardar
 @Component({
   selector: 'app-perfil-info',
   standalone: true,
-  imports: [InputBoxComponent, CommonModule, InputComponent,FormsModule,BtnGuardarCancelarComponent],
+  imports: [InputBoxComponent, CommonModule, InputComponent, FormsModule, BtnGuardarCancelarComponent],
   templateUrl: './perfil-info.component.html',
   styleUrl: './perfil-info.component.css'
 })
 export class PerfilInfoComponent {
   @HostBinding('style.width') width: string = '100%';
 
-  perfilForm:FormGroup;
-  
+  perfilForm: FormGroup;
 
+  private chPermitidosNomb = '[a-zA-Z]*$'
+  private chPermitidosUser = '^[a-zA-Z0-9]*$'
   mostrarCalculador = new MostrarCalculador();
-  
-  mostrarNuevosInputs: boolean = false; 
+
+  mostrarNuevosInputs: boolean = false;
 
   boton = new BtnGuardarCancelarComponent()
 
+
+
+  constructor(private fb: FormBuilder) {
+    this.perfilForm = this.fb.group({
+      'nombre': ['', [Validators.required, Validators.pattern(this.chPermitidosNomb)]],
+      'apellido': ['', [Validators.required, Validators.pattern(this.chPermitidosNomb)]],
+      'username': ['', [Validators.required, Validators.pattern(this.chPermitidosUser)]],
+      'fecha de nacimiento': ['', [Validators.required, DateValidator.LessThanToday]],
+      'email': ['', [Validators.required, Validators.email]]
+    })
+  }
+
+
+  
   mostrar(event: any) {
     this.mostrarNuevosInputs = event.target.checked; // Actualiza según si el checkbox está marcado o no
   }
 
-
-  constructor(private fb : FormBuilder){
-    this.perfilForm = this.fb.group({
-      'nombre de usuario':[],
-      'apellido':[],
-      'fecha de nacimiento':[],
-      'email':[]
-    })
+  errorMessage(form:string){
+    var error = this.fb.control(form).errors
+    console.log(error)
   }
-  
 }
 
-class MostrarCalculador{
-  mostrar:boolean = false
+class MostrarCalculador {
+  mostrar: boolean = false
 
-  show(){
+  show() {
     this.mostrar = !this.mostrar
   }
 }
+
+export class DateValidator {
+
+  static LessThanToday(control: FormControl): ValidationErrors | null {
+    let hoy: Date = new Date();
+
+    if (new Date(control.value) > hoy)
+      return { "LessThanToday": true };
+
+    return null;
+  }
+}
+
+
+
