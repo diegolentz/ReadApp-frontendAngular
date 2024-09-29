2923e541ff70ca5b16fc91c41db38345071277ac
ValentinPg <105302328+ValentinPg@users.noreply.github.com>
2024-09-29 18:44:50 -0300

    Validacion de formulario de calculador hecha

--- src/app/shared/perfil-info/perfil-info.component.ts
+++ src/app/shared/perfil-info/perfil-info.component.ts
@@ -2,7 +2,7 @@ import { Component, HostBinding } from '@angular/core';
 import { InputBoxComponent } from "../input-box/input-box.component";
 import { CommonModule } from '@angular/common';
 import { InputComponent } from '../../input/input.component';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BtnGuardarCancelarComponent } from '../btn-guardar-cancelar/btn-guardar-cancelar.component';
 
@@ -19,9 +19,12 @@ export class PerfilInfoComponent {
   @HostBinding('style.width') width: string = '100%';
 
   perfilForm: FormGroup;
+  calculadorForm: FormGroup;
 
   private chPermitidosNomb = '[a-zA-Z]*$'
   private chPermitidosUser = '^[a-zA-Z0-9]*$'
+
+  minLectura: number = 0;
   mostrarCalculador = new MostrarCalculador();
 
   mostrarNuevosInputs: boolean = false;
@@ -38,27 +41,43 @@ export class PerfilInfoComponent {
       'fecha de nacimiento': ['', [Validators.required, DateValidator.LessThanToday]],
       'email': ['', [Validators.required, Validators.email]]
     })
+
+    this.calculadorForm = this.fb.group({
+      'numero min': [0, [Validators.min(0)]],
+      'numero max': [0, [Validators.min(0)]]
+    })
+
+    this.calculadorForm.setValidators(MinMaxValidator.LessThanMin())
   }
 
 
-  
+
   mostrar(event: any) {
     this.mostrarNuevosInputs = event.target.checked; // Actualiza según si el checkbox está marcado o no
   }
 
-  errorMessage(form:string){
+  errorMessage(form: string) {
     var error = this.fb.control(form).errors
     console.log(error)
   }
 
-  ngOnInit(){
+  ngOnInit() {
     Object.keys(this.perfilForm.controls).forEach(nombreForm => {
       var form = this.perfilForm.get(nombreForm)
       form?.statusChanges.subscribe(valor => {
         console.log(valor)
       })
     })
+
+    Object.keys(this.calculadorForm.controls).forEach(nombreForm => {
+      var form = this.calculadorForm.get(nombreForm)
+      form?.statusChanges.subscribe(valor => {
+        console.log(valor)
+      })
+    })
   }
+
+
 }
 
 class MostrarCalculador {
@@ -78,6 +97,27 @@ export class DateValidator {
       return { "LessThanToday": true };
 
     return null;
+  }
+}
+
+export class MinMaxValidator {
+
+  static LessThanMin(): any {
+    return (group: FormGroup) => {
+      const minControl = group.get('numero min')
+      const maxControl = group.get('numero max')
+
+      let min: number = minControl?.value
+      let max: number = maxControl?.value
+
+      /* console.log("El minimo es:", min)
+      console.log("El maximo es:", max) */
+
+      if (min > max && max < min)
+        {maxControl?.setErrors({ "LessThanMin": true });}
+      else
+      {minControl?.setErrors(null);}
+    }
   }
 }
 
