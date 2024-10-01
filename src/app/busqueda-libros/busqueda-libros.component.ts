ab49048b19ae2493fe30a6fc5a938d84df2cc26e
diegolentz <diegoolentz@gmail.com>
2024-10-01 10:36:09 -0300

    mejorando el manejo con el nav

--- src/app/busqueda-libros/busqueda-libros.component.ts
+++ src/app/busqueda-libros/busqueda-libros.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { BookService } from '../../service/book.service';
 import { Book } from '../../domain/book';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../shared/header/header.component';
 import { NavComponent } from '../nav/nav.component';
 import { ContenedorSectionComponent } from '../contenedor-section/contenedor-section.component';
@@ -17,21 +17,22 @@ import { ContainerBooksComponent } from '../shared/layouts/books/books.component
 })
 export class BusquedaLibrosComponent {
   books: Book[] = [];
+  allBooks: Book[] = [];
 
   constructor(public bookService: BookService) {}
 
   async ngOnInit() {
     // me traigo los libros
-    this.books = await this.bookService.obtenerLibros();
+    this.allBooks = await this.bookService.obtenerLibros();
+    this.books = this.allBooks;
 
     // me subscribo a los cambios para filtrar
     this.bookService.filtroCambiado.subscribe(
-      async (nuevoFiltro: string) => {this.books = await this.bookService.obtenerLibros();
-      if (nuevoFiltro) {
-        this.books = this.books.filter((book) =>
-          book.titulo.toLowerCase().includes(nuevoFiltro.toLowerCase())
-        );
+      (nuevoFiltro: string) => {
+        this.books = nuevoFiltro ? 
+          this.allBooks.filter((book) => book.titulo.toLowerCase().includes(nuevoFiltro.toLowerCase()) || book.author.toLowerCase().includes(nuevoFiltro.toLowerCase())) : 
+          this.allBooks;
       }
-    });
+    );
   }
 }
