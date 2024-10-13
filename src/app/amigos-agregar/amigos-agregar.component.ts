f1f9cfd032aef9df70885f1184843f632df18fed
borrellidelfina <borrellidelfina@gmail.com>
2024-10-13 15:38:33 -0300

    Update BotonAgregar
    El boton agregar recibe dos inputs de su componente padre que se utilizan para generar la vista donde se agrega algo. Antes solo se podia agregar libros, la idea a partir de este doble input es que este boton nos lleve a agregar cualquier objeto que haga falta.

---
+++ src/app/amigos-agregar/amigos-agregar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-amigos-agregar',
+  standalone: true,
+  imports: [],
+  templateUrl: './amigos-agregar.component.html',
+  styleUrl: './amigos-agregar.component.css'
+})
+export class AmigosAgregarComponent {
+
+}
