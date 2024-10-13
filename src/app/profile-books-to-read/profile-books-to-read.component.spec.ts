74ee60d520cd5e9638872baa5f0d6d9458bddb91
AdrianPerez0306 <perezadrian.m@hotmail.com>
2024-10-13 18:39:52 -0300

    Login terminado y testeado. Falta acomodar errores lindos y separar formularios en componentes

--- src/app/profile-books-to-read/profile-books-to-read.component.spec.ts
+++ src/app/profile-books-to-read/profile-books-to-read.component.spec.ts
@@ -2,16 +2,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileBooksToReadComponent } from './profile-books-to-read.component';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 describe('ProfileBooksToReadComponent', () => {
   let component: ProfileBooksToReadComponent;
   let fixture: ComponentFixture<ProfileBooksToReadComponent>;
   let httpClientSpy: jasmine.SpyObj<HttpClient>
+  let activatedRouteSpy: jasmine.SpyObj<ActivatedRoute>
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProfileBooksToReadComponent],
       providers: [
-        {provide: HttpClient, useValue: httpClientSpy}
+        {provide: HttpClient, useValue: httpClientSpy},
+        {provide: ActivatedRoute, useValue: activatedRouteSpy}
       ]
     })
     .compileComponents();
--- src/app/shared/shorcut-my-profile/shorcut-my-profile.component.spec.ts
+++ src/app/shared/shorcut-my-profile/shorcut-my-profile.component.spec.ts
@@ -15,7 +15,6 @@ describe('ShorcutMyProfileComponent', () => {
 
     fixture = TestBed.createComponent(ShorcutMyProfileComponent);
     component = fixture.componentInstance;
-    component.user = new User()
     fixture.detectChanges();
   });
 
