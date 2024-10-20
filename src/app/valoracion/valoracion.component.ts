b8db769fe98a7fd38d54dbf32b993b79251dad4c
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2024-10-20 00:12:50 -0300

    valoracion con estrellas

--- src/app/valoracion/valoracion.component.ts
+++ src/app/valoracion/valoracion.component.ts
@@ -6,11 +6,13 @@ import { NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BtnGuardarCancelarComponent } from "../shared/btn-guardar-cancelar/btn-guardar-cancelar.component";
 import { RecommendationService } from '../../service/recommendation.service';
-
+import { ServiceUser } from '../../service/service-user.service';
+import { UserBasic } from '../../domain/tmpUser';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-valoracion',
   standalone: true,
-  imports: [BotonAgregarComponent, ValoracionIndividualComponent, NgIf, FormsModule, BtnGuardarCancelarComponent],
+  imports: [BotonAgregarComponent, ValoracionIndividualComponent, NgIf, FormsModule, BtnGuardarCancelarComponent,CommonModule],
   templateUrl: './valoracion.component.html',
   styleUrls: ['./valoracion.component.css']
 })
@@ -19,13 +21,21 @@ export class ValoracionComponent implements OnInit {
   @Input() score!: number;
   @Input() puedeValorar!: boolean;
   @Input() id!: number;
-  
-  agregarValoracion: boolean = false;
-  nuevaValoracion: Valoration = new Valoration("", "", 0, new Date(1914, 14, 14), ""); 
-  constructor(private recommendationService: RecommendationService) {}
 
-  ngOnInit() {
+  user:UserBasic = new UserBasic();
+  agregarValoracion: boolean = false;
+  rating = 0
+  nuevaValoracion: Valoration = new Valoration(this.user.fotoPath, this.user.username , 0 , new Date(1914, 14, 14), ""); 
+
+  constructor(private recommendationService: RecommendationService,private userService:ServiceUser ) {}
+
+  async ngOnInit() {
     
+  }
+
+  setRating(valor:number){
+    this.rating = valor
+    this.nuevaValoracion.valor = this.rating
   }
 
   puedeAgregarValoracion(): any {
@@ -36,6 +46,7 @@ export class ValoracionComponent implements OnInit {
     console.log(this.nuevaValoracion)
     await this.recommendationService.agregarValoracion(this.nuevaValoracion,this.id)
     await this.cancelar()
+    await window.location.reload()
   }
 
   agregaValoracion() {
