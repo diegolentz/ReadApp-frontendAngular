fec89d7b53598bb93304470dbd93f3c211de5f99
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2024-10-20 03:05:01 -0300

    mejorando el css

--- src/app/valoracion/valoracion.component.ts
+++ src/app/valoracion/valoracion.component.ts
@@ -9,50 +9,60 @@ import { RecommendationService } from '../../service/recommendation.service';
 import { ServiceUser } from '../../service/service-user.service';
 import { UserBasic } from '../../domain/tmpUser';
 import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-valoracion',
   standalone: true,
-  imports: [BotonAgregarComponent, ValoracionIndividualComponent, NgIf, FormsModule, BtnGuardarCancelarComponent,CommonModule],
+  imports: [BotonAgregarComponent, ValoracionIndividualComponent, NgIf, FormsModule, BtnGuardarCancelarComponent, CommonModule],
   templateUrl: './valoracion.component.html',
   styleUrls: ['./valoracion.component.css']
 })
 export class ValoracionComponent implements OnInit {
-  @Input() valoraciones!: Array<Valoration>; 
-  @Input() score!: number;
-  @Input() puedeValorar!: boolean;
-  @Input() id!: number;
+  @Input() valoraciones!: Array<Valoration>
+  @Input() score!: number
+  @Input() puedeValorar!: boolean
+  @Input() id!: number
 
-  user:UserBasic = new UserBasic();
-  agregarValoracion: boolean = false;
+  user: UserBasic = new UserBasic()
+  agregarValoracion: boolean = false
   rating = 0
-  nuevaValoracion: Valoration = new Valoration(this.user.fotoPath, this.user.username , 0 , new Date(1914, 14, 14), ""); 
+  nuevaValoracion: Valoration = new Valoration(this.user.fotoPath, this.user.username, 0, new Date(1914, 14, 14), "") 
 
-  constructor(private recommendationService: RecommendationService,private userService:ServiceUser ) {}
+  constructor(private recommendationService: RecommendationService, private userService: ServiceUser, private toast: ToastrService) {}
 
-  async ngOnInit() {
-    
-  }
+  async ngOnInit() {}
 
-  setRating(valor:number){
+  setRating(valor: number) {
     this.rating = valor
-    this.nuevaValoracion.valor = this.rating
+  }
+  async agregarLaValoracion() {
+    if (this.validacion()) {
+      this.toast.warning("Por favor complete los campos vacios");
+      return;
+    }
+  
+    this.nuevaValoracion.valor = this.rating;
+  
+    await this.recommendationService.agregarValoracion(this.nuevaValoracion, this.id);
+    await this.cancelar();
+    await this.toast.success("Se agregó correctamente la valoración");
+  
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
   }
 
-  puedeAgregarValoracion(): any {
-    throw new Error('Method not implemented.');
-  }
-
-  async agregarLaValoracion(){
-    console.log(this.nuevaValoracion)
-    await this.recommendationService.agregarValoracion(this.nuevaValoracion,this.id)
-    await this.cancelar()
-    await window.location.reload()
-  }
+  validacion = (): boolean => !this.nuevaValoracion.comentario.trim() || this.rating === 0
 
   agregaValoracion() {
-    this.agregarValoracion = true; 
+    this.agregarValoracion = true
+    this.puedeValorar = false
   }
+
   cancelar() {
-    this.agregarValoracion = false; 
+    this.agregarValoracion = false
+    this.nuevaValoracion.comentario = ""
+    this.rating = 0
   }
 }
