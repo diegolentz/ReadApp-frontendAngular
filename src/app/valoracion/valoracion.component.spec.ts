64226e56800cae7b81ead26230424f0052651d45
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2024-10-22 01:01:40 -0300

    se agregaron 3 test al componete valoraciones

--- src/app/valoracion/valoracion.component.spec.ts
+++ src/app/valoracion/valoracion.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ValoracionComponent } from './valoracion.component';
+import { RecommendationService } from '../../service/recommendation.service';
+import { ToastrService } from 'ngx-toastr';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http'; // Importa HttpClientModule
 
 describe('ValoracionComponent', () => {
   let component: ValoracionComponent;
@@ -8,16 +12,42 @@ describe('ValoracionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ValoracionComponent]
-    })
-    .compileComponents();
+      imports: [
+        CommonModule, 
+        HttpClientModule, // Agrega HttpClientModule aquí
+        ValoracionComponent
+      ],
+      providers: [
+        { provide: RecommendationService, useValue: {} }, // Mock del servicio
+        { provide: ToastrService, useValue: {} } // Mock del toastr
+      ],
+      schemas: [NO_ERRORS_SCHEMA] // Ignorar errores de componentes no declarados
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ValoracionComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('debería crear el componente', () => {
     expect(component).toBeTruthy();
+  });
+  it('debería establecer la calificación correctamente al llamar a setRating', () => {
+    component.setRating(4);
+    expect(component.rating).toBe(4);
+  });
+  it('debería agregar la valoración correctamente y reiniciar el estado', async () => {
+    // Simula la entrada del usuario
+    component.nuevaValoracion.comentario = 'Buen libro';
+    component.rating = 5;
+  
+    // Simula que el servicio devuelve una promesa resuelta
+    const recommendationService = TestBed.inject(RecommendationService);
+    recommendationService.agregarValoracion = jasmine.createSpy('agregarValoracion').and.returnValue(Promise.resolve());
+  
+    await component.agregarLaValoracion();
+  
+    expect(recommendationService.agregarValoracion).toHaveBeenCalledWith(component.nuevaValoracion, component.id);
+    expect(component.nuevaValoracion.comentario).toBe('');
+    expect(component.rating).toBe(0);
   });
 });
