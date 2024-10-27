1afef77e097a65c6afbf926cf0b2682cf07a3433
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2024-10-27 14:08:54 -0300

    arreglo test shortcut-myprfile

--- src/app/shared/shorcut-my-profile/shorcut-my-profile.component.spec.ts
+++ src/app/shared/shorcut-my-profile/shorcut-my-profile.component.spec.ts
@@ -1,27 +1,63 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ShorcutMyProfileComponent } from './shorcut-my-profile.component';
-import { User } from '../../../domain/user';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ServiceUser } from '../../../service/service-user.service';
+import { UserBasic } from '../../../domain/tmpUser';
 
 describe('ShorcutMyProfileComponent', () => {
   let component: ShorcutMyProfileComponent;
   let fixture: ComponentFixture<ShorcutMyProfileComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ShorcutMyProfileComponent]
-    })
-    .compileComponents();
+      imports: [
+        ShorcutMyProfileComponent,
+        HttpClientTestingModule,
+        RouterTestingModule // Para simular el enrutador
+      ],
+      providers: [
+        {
+          provide: ServiceUser,
+          useValue: jasmine.createSpyObj('ServiceUser', ['getUser']) // Simula el servicio
+        }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ShorcutMyProfileComponent);
     component = fixture.componentInstance;
 
-    /* component.user = new User() */
+    // Inicializa los inputs
+    component.user = new UserBasic();
+    component.user.fotoPath = 'path/to/photo.jpg'; // Si tienes la propiedad fotoPath
+    component.show = true; // Establece un valor para show
 
-    fixture.detectChanges();
+    fixture.detectChanges(); // Detecta cambios para inicializar el componente
+
+    // Inyecta el Router para espiar su método navigate
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('debería alternar displayShorcut cuando se llame a changeDisplay', () => {
+    expect(component.displayShorcut).toBeFalse(); // Inicialmente debería ser false
+    component.changeDisplay(); // Cambia el estado
+    expect(component.displayShorcut).toBeTrue(); // Ahora debería ser true
+    component.changeDisplay(); // Cambia de nuevo
+    expect(component.displayShorcut).toBeFalse(); // Debería volver a ser false
+  });
+
+  it('debería navegar al camino correcto al llamar a goTo', () => {
+    spyOn(router, 'navigate'); // Espía el método navigate
+
+    const optionPath = 'my-profile';
+    component.goTo(optionPath); // Llama a goTo
+
+    expect(router.navigate).toHaveBeenCalledWith([optionPath]); // Verifica que se navegue al camino correcto
   });
 });
