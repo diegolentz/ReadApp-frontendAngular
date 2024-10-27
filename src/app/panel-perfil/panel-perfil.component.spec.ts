0c72761073cb239f33bbd486611f16db961a16f1
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2024-10-27 14:12:55 -0300

    arreglo test panel-perfil

--- src/app/panel-perfil/panel-perfil.component.spec.ts
+++ src/app/panel-perfil/panel-perfil.component.spec.ts
@@ -1,19 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PanelPerfilComponent } from './panel-perfil.component';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ToastrModule } from 'ngx-toastr'; // Importa ToastrModule
 
 describe('PanelPerfilComponent', () => {
   let component: PanelPerfilComponent;
   let fixture: ComponentFixture<PanelPerfilComponent>;
-  let activatedRouteSpy: jasmine.SpyObj<ActivatedRoute>
+  let activatedRouteSpy: jasmine.SpyObj<ActivatedRoute>;
+
   beforeEach(async () => {
+    activatedRouteSpy = jasmine.createSpyObj('ActivatedRoute', ['snapshot']); // Inicializa el espía
+
     await TestBed.configureTestingModule({
       imports: [
         PanelPerfilComponent,
+        HttpClientTestingModule, // Agrega el módulo de pruebas de HttpClient
+        ToastrModule.forRoot() // Importa ToastrModule aquí
       ],
       providers: [
-        {provide: ActivatedRoute, useValue: activatedRouteSpy}
+        { provide: ActivatedRoute, useValue: activatedRouteSpy }
       ]
     })
     .compileComponents();
