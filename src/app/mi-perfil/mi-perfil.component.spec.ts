674cfb905675dbc03b9a560a1de0aa6e66d2075a
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2024-10-27 13:58:39 -0300

    arreglo primer test

--- src/app/mi-perfil/mi-perfil.component.spec.ts
+++ src/app/mi-perfil/mi-perfil.component.spec.ts
@@ -1,17 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MiPerfilComponent } from './mi-perfil.component';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('MiPerfilComponent', () => {
   let component: MiPerfilComponent;
   let fixture: ComponentFixture<MiPerfilComponent>;
-  let activatedRouteSpy: jasmine.SpyObj<ActivatedRoute>
+  let activatedRouteSpy: jasmine.SpyObj<ActivatedRoute>;
+
   beforeEach(async () => {
+    activatedRouteSpy = jasmine.createSpyObj('ActivatedRoute', ['snapshot']);
+
     await TestBed.configureTestingModule({
-      imports: [MiPerfilComponent],
+      imports: [
+        MiPerfilComponent,
+        HttpClientTestingModule,
+        ToastrModule.forRoot()  // Importa ToastrModule aquí
+      ],
       providers: [
-        {provide: ActivatedRoute, useValue: activatedRouteSpy}
+        { provide: ActivatedRoute, useValue: activatedRouteSpy }
       ]
     })
     .compileComponents();
